0c26412a0198cd2326de5c176eb2ea2f8403464e
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-08-16 23:19:18 +0700

    refactor: improve river generation with neighbor cells

--- map.py
+++ map.py
@@ -1,5 +1,7 @@
+from utils import rand
 from utils import randbool
 from utils import randcell
+from utils import get_neighbor
 
 # 0 - поле
 # 1 - дерево
@@ -15,18 +17,35 @@ class Map:
     self.height = height
     self.cells = [[0 for i in range(width)]for j in range(height)]
 
-  def generate_river(self, length):
-    rand_cell = randcell(self.width, self.height)
-    rand_x, rand_y = rand_cell[0], rand_cell[1]
+  def generate_river(self, max_length):
+    while True:
+      rand_x, rand_y = randcell(self.width, self.height)
+
+      if self.cells[rand_x][rand_y] != 2:
+        break
+    
     self.cells[rand_x][rand_y] = 2
+    max_length -= 1
+
+    while max_length > 0:
+      free_neighbors = []
+
+      for x, y in get_neighbor(rand_x, rand_y):
+        if self.check_bounds(x, y) and self.cells[x][y] != 2:
+          free_neighbors.append((x, y))
+
+      if not free_neighbors:
+        break
+
+      rand_x, rand_y = free_neighbors[rand(0, len(free_neighbors) - 1)]
+      self.cells[rand_x][rand_y] = 2
+      max_length -= 1      
 
   def generate_forest(self, threshold, max_random):
     for i in range(self.height):
       for j in range(self.width):
         if randbool(threshold, max_random):
           self.cells[i][j] = 1
-
-  # def generate_forest():
 
   def print_map(self):
     print("⬛" * (self.width + 2))
@@ -45,4 +64,6 @@ class Map:
 
 tmp = Map(20, 10)
 tmp.generate_river(10)
+tmp.generate_river(5)
+tmp.generate_river(8)
 tmp.print_map()
--- utils.py
+++ utils.py
@@ -4,7 +4,15 @@ def randbool(threshold, max_random):
   return rand(0, max_random) <= threshold
 
 def randcell(width, height):
-  rand_width = rand(0, width)
-  rand_height = rand(0, height)
+  rand_x = rand(0, height - 1)
+  rand_y = rand(0, width - 1)
+  return (rand_x, rand_y) 
 
-  return (rand_height, rand_width) 
+# 0 - ввеерх, 1- направо? 2 - вниз, 3 - налево
+def get_neighbor(x, y):
+  return [
+    (x - 1, y),
+    (x, y + 1),
+    (x + 1, y),
+    (x, y - 1)
+  ]
